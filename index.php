c4d972bc4307d1d66aa0ee06242d981af5ad0a88
Yuri do Monte <yspm.developer@gmail.com>
2024-01-23 14:02:42 -0300

    initial session finished

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ $password = 'senha';
 $apptoken = 'apptoken';
 
 $glpiApi = new GLPIApi($apiUrl, $username, $password, $apptoken);
-//var_dump($glpiApi);
 
 echo $glpiApi->initSession();
 
-//echo $glpiApi->getTicket(1);
